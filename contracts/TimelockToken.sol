9a738921e7fd62e8a29ea55fbd7f4d2462704c88
Trí Lê <letri234@outlook.com>
2022-04-08 17:27:14 +0700

    upload timelocktoken

--- contracts/TimelockToken.sol
+++ contracts/TimelockToken.sol
@@ -1,56 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Token.sol";
 
-contract Token is ERC20, ERC20Burnable, Ownable {
-    address crowdsale;
-    address timelock;
+contract TimelockToken {
     
-    constructor() ERC20("MyToken", "MTK") {
-        _mint(msg.sender, 10_000_000 * 10 ** decimals());
+    uint public start;
+    uint public end;
+    uint totalTokens;
+    
+    Token erc20Token;
+    
+    mapping(address => uint) public deposits;
+    constructor(address _token) {
+        erc20Token = Token(_token);
     }
+    
+    function setStarttoEnd(uint _start, uint _amountTime) external {
+        start = _start;
+        end = start + _amountTime;
+    }
+    
+    function deposit(address _from, address _beneficiary, uint _amount) external {
+        require(block.timestamp >= start && block.timestamp < end, "Time is out of range");
+        deposits[_beneficiary] += _amount;
+        totalTokens+= _amount;
+        erc20Token.transferToTimeLock(_from, address(this) ,_amount);
+    }   
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
-    }
-    
-    function setCrowdsale(address _crowdsale, address _timelock) public onlyOwner {
-        crowdsale = _crowdsale;
-        timelock = _timelock;
-    }
-    
-    modifier onlyCrowsale() {
-        require(msg.sender == crowdsale, "Only the crowdsale can modify the vault");
-        _;
-    }
-
-    modifier onlyTimelock() {
-        require(msg.sender == timelock, "Not timelock address");
-        _;
-    }
-    
-    
-    function transferToTimeLock(address _owner, address _to,uint256 _amount) external onlyTimelock {
-        _transfer(_owner, _to, _amount);
-        _approve(_owner, _to, allowance(_owner, _to) + _amount);
-        emit Transfer(_owner, _to, _amount);
+    function withdraw(address _beneficiary) external {
+        require(block.timestamp >= end, "Not ended yet");
+        require(deposits[_beneficiary] > 0, "No deposit");
+        uint amountToken = deposits[_beneficiary];
+        deposits[_beneficiary] = 0;
+        erc20Token.transfer(_beneficiary, amountToken);
+        erc20Token.approve(_beneficiary, amountToken);
     }
 
-    function burnTimelockToken(address account, uint amount) external onlyTimelock {
-        require(account != address(0), "Non zere account");
-        address owner = owner();
-        require(allowance(owner, account) >= amount);
-        _burn(account, amount);
-        _approve(owner, account, allowance(owner, account) - amount);
-        } 
-    
-    function burnExcessTokenAfterSale(address _owner, uint256 _amount) external onlyCrowsale {
-        _burn(_owner, _amount);
-        _spendAllowance(_owner, _owner, _amount);
-        emit Transfer(_owner, address(0), _amount);
+    function burnToken(address investor) external {
+        address account = address(this);
+        uint accountBalance = erc20Token.balanceOf(account);
+        deposits[investor] = 0;
+        erc20Token.burnTimelockToken(account, accountBalance);
     }
+
+
     
 }
